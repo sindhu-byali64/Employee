b8106377c96b303ac98071b42f684de70599d692
sindhu-byali64 <sindhutbyali6@gmail.com>
2025-05-07 15:22:54 +0530

    save

---
+++ new/operation.java
@@ -0,0 +1,9 @@
+public class operation{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("subtraction:"+(a+b));
+        System.out.println("multiplication:"+(a+b));
+    }
+}
